c65c529e98b5d8342ea88d8bb220d462e7bdad13
Bryam <brayanamaya406@gmail.com>
2025-03-17 15:10:53 +0100

    Actulizacion 1.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom"; // <-- HashRouter en lugar de BrowserRouter
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/basic/Navbar";
 import Footer from "./components/basic/footer";
 import "./App.css";
